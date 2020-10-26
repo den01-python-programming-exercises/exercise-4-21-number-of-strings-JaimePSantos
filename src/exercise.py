56266a4e514c1a190c430e14edec3cf57809cbcd
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-26 12:41:31 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
+    strReadNo = 0
+    while(True):
+      strRead = input()
+      if(strRead=="end"):
+        break
+      strReadNo +=1
+    print(strReadNo)
 
 if __name__ == '__main__':
     main()
